03cc7256f4ff0c98c7960a5aaf667a6ef95ccd96
Kevin Hang <herezy2006@gmail.com>
2024-06-17 04:40:44 +0200

    start navbar need to finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xlw-full">
+        <FloatingNav
+          navItems={[
+            {name: "string", link: "/", icon: <FaHome/>}
+          ]} />
         <Hero/>
       </div>
     </main>
