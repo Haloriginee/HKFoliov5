a0cfd54fa1956e0554f45e07c6ec9253c96356ab
Kevin Hang <herezy2006@gmail.com>
2024-06-25 09:28:04 +0200

    fixing things

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
               <img
                 src={profile.img}
-                alt={profile.id}
+                alt="work or not"
                 width={20}
                 height={20}
               />
