92d773a3d8ea195b67ef6cba7d5aa04e19ca8942
Kevin Hang <herezy2006@gmail.com>
2024-06-30 14:03:30 +0200

    fixed some link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,12 +30,14 @@ const Footer = () => {
         <div className='flex items-center md:gap-3 gap-6'>
           {socialMedia.map((profile) => (
             <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-              <img
-                src={profile.img}
-                alt="work or not"
-                width={20}
-                height={20}
-              />
+              <a href={profile.link} target='_blank'>
+                <img
+                  src={profile.img}
+                  alt="work or not"
+                  width={20}
+                  height={20}
+                />
+              </a>
             </div>
           ))}
         </div>
--- data/index.ts
+++ data/index.ts
@@ -105,7 +105,7 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Mobile App Dev - JS Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2",
     thumbnail: "/exp2.svg",
@@ -130,9 +130,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/Haloriginee",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/k%C3%A9vin-hang/",
   },
 ];
